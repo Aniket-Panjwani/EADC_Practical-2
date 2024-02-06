a7dcfb55564d6635d5b3052ae9ac1885ccd8a0ec
ANIKET PANJWANI <aniketpanjwani20@gmail.com>
2024-02-06 08:42:55 +0530

    testing changes for AWS Codepipline

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello, this is a code by Aniket to test pipeline and this is the second version');
+    // res.send('Hello, this is a code by Aniket to test pipeline and this is the second version');
     res.send('Hello, this is the second version to test AWS codepipline i have made changes in vs code and pushed to github it will automaticaly publice the changes to the AWS Elasticbeansstalk using code pipline');
   });
   const port = process.env.PORT || 3000;
